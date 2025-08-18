b2d0e9c887735db2a3de45912243b1fd57d6b117
Ananya Kundu <ananyak091@gmail.com>
2025-08-18 22:34:19 +0530

    I have changed it.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const App = () => {
-  return <div>App TuneMood</div>;
+  return (
+    <div>
+      App TuneMood. This is our Music Reccomendation system, built in React,
+      MongoDB, Node, Express with ML Model for prediction.
+    </div>
+  );
 };
 
 export default App;
