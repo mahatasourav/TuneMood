327ac987355fbe773dd84772bb851cb31a455f71
SOURAV MAHATA <mahatasourav35@gmail.com>
2025-08-23 20:31:01 +0530

    Makes some folder

---
+++ frontend/src/components/UpperNavbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const UpperNavbar = () => {
+  return (
+    <div>
+      <div>
+        {/* Search Section  */}
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default UpperNavbar;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import UpperNavbar from "../components/UpperNavbar";
+import HeroSection from "../components/HeroSection";
+import Playlist from "../components/Playlist";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col gap-6 p-2">
+      <UpperNavbar />
+      <HeroSection />
+      <Playlist />
+    </div>
+  );
 };
 
 export default Home;
