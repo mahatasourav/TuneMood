c0780344b8ca166977edf6b54f877ca894cf4c69
SOURAV MAHATA <mahatasourav35@gmail.com>
2025-09-04 00:42:04 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+
+import connectDB from "./config/mongodb.js";
+import dotenv from "dotenv";
+// import connectCloudinary from "./config/cloudinary.js";
+
+dotenv.config();
+
+const app = express();
+const port = 4000;
+connectDB();
+// middlewares
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*", // or restrict to your frontend Render URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("api working ");
+});
+
+//localhost:4000/api/admin
+
+app.listen(port, () => {
+  console.log("server started at", "http://localhost:4000");
+});
+
+dotenv.config();
