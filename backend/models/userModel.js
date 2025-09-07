f80d3af5d16518426619f440650ba9a812e365a6
SOURAV MAHATA <mahatasourav35@gmail.com>
2025-09-07 22:16:16 +0530

    routing setup done and database design compledted userSchema created

---
+++ backend/models/userModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    favourites: [
+      {
+        playlistId: String,
+        title: String,
+        artist: String,
+        moodTag: String,
+      },
+    ],
+    moodHistory: [
+      {
+        mood: String,
+        songId: String, // spotify track id
+        listenedAt: { type: Date, default: Date.now },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
+
+// Mood, Favourite, Prediction
